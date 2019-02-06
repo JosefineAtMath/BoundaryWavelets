0258c7febb388fb0fe2166a3e4551f8739136f6e
Thomas Arildsen <tha@es.aau.dk>
2019-02-06 22:46:50 +0100

    WIP: de-linting according to `pycodestyle`
    Still need to test that no errors were introduced.

--- test/DataTest.py
+++ test/DataTest.py
@@ -2,7 +2,7 @@
 """
 This module is used for testing the boundary wavelets on ECG data.
 
-The BoundaryWavelets.py package is licensed under the MIT "Expat" 
+The BoundaryWavelets.py package is licensed under the MIT "Expat"
 Copyright (c) 2019: Josefine Holm and Steffen L. Nielsen.
 """
 
@@ -12,20 +12,23 @@ import pywt
 import matplotlib.pyplot as plt
 import ReconFunctions as RF
 
-def TestPlot(Name='Data',Row=1,Section=214,J=7,N=12,Wavelet='db3'):
+
+def TestPlot(Name='Data', Row=1, Section=214, J=7, N=12, Wavelet='db3'):
     '''
-    This function makes decompositions and reconstructions of a chosen section 
+    This function makes decompositions and reconstructions of a chosen section
     of the data, bodt with boundary wavelets and with mirrored extension. The
-    differance between the orignal signal and the two reconstructions are 
-    calculated and printed and all three signals are plotted in the same figure.
+    difference between the orignal signal and the two reconstructions are
+    calculated and printed and all three signals are plotted in the
+    same figure.
+
     INPUT:
         Name:
             str: The MATLAB data file from whichto load.
         Row:
             int: The row in the dataset to use.
         Section:
-            int: Which section of the data to use. The samples that will be used
-            are: [Section*2**N:Section*2**N+2**N]
+            int: Which section of the data to use. The samples that
+            will be used are: [Section*2**N:Section*2**N+2**N]
         J:
             int: The scale.
         N:
@@ -33,34 +36,36 @@ def TestPlot(Name='Data',Row=1,Section=214,J=7,N=12,Wavelet='db3'):
         Wavelet:
             THe name of the wavelet to be used. eg: 'db2'.
     '''
-    data       = sp.loadmat(Name)
-    phi        = pywt.Wavelet(Wavelet).wavefun(level=14)[0][1:]
-    phi        = phi[::2**(14-N)]
-    Signal     = data['val'][Row,Section*2**N:Section*2**N+2**N]
-    x1         = RF.DecomBoundary(Signal,J,Wavelet,phi,N=N)
+    data = sp.loadmat(Name)
+    phi = pywt.Wavelet(Wavelet).wavefun(level=14)[0][1:]
+    phi = phi[::2**(14-N)]
+    Signal = data['val'][Row, Section*2**N:Section*2**N+2**N]
+    x1 = RF.DecomBoundary(Signal, J, Wavelet, phi, N=N)
     print(len(x1))
-    NewSignal1 = np.real(RF.ReconBoundary(x1,J,Wavelet,phi,N=N))
-    x2         = RF.DecomMirror(Signal,J,Wavelet,phi,N=N)
-    NewSignal2 = np.real(RF.ReconMirror(x2,J,Wavelet,phi,N=N))
-    dif1       = np.sum(np.abs(Signal-NewSignal1)**2)**(1/2)/2**N
-    dif2       = np.sum(np.abs(Signal-NewSignal2)**2)**(1/2)/2**N
-    print(dif1,dif2)
-    
+    NewSignal1 = np.real(RF.ReconBoundary(x1, J, Wavelet, phi, N=N))
+    x2 = RF.DecomMirror(Signal, J, Wavelet, phi, N=N)
+    NewSignal2 = np.real(RF.ReconMirror(x2, J, Wavelet, phi, N=N))
+    dif1 = np.sum(np.abs(Signal-NewSignal1)**2)**(1/2)/2**N
+    dif2 = np.sum(np.abs(Signal-NewSignal2)**2)**(1/2)/2**N
+    print(dif1, dif2)
+
     plt.figure()
-    plt.plot(Signal,label='Original')
-    plt.plot(NewSignal1,label='Boundary wavelets')
-    plt.plot(NewSignal2,label='Mirror')
+    plt.plot(Signal, label='Original')
+    plt.plot(NewSignal1, label='Boundary wavelets')
+    plt.plot(NewSignal2, label='Mirror')
     plt.xlabel('Sample index')
     plt.legend()
     return
 
-def Test(Name='Data',Row=1,J=7,N=12,Wavelet='db3'):
+
+def Test(Name='Data', Row=1, J=7, N=12, Wavelet='db3'):
     '''
-    This function makes decompositions and reconstructions of several sections 
+    This function makes decompositions and reconstructions of several sections
     of the data, both with boundary wavelets and with mirrored extension. The
-    differance between the orignal signal and the two reconstructions are 
+    differance between the orignal signal and the two reconstructions are
     calculated. The test is run for as many disjoint sections of the signal
     as possible.
+
     INPUT:
         Name:
             str: The MATLAB data file from whichto load.
@@ -74,35 +79,38 @@ def Test(Name='Data',Row=1,J=7,N=12,Wavelet='db3'):
             THe name of the wavelet to be used. eg: 'db2'.
     OUTPUT:
         Result:
-            float64: 2D array. the firt row is the difference between the original
-            signal and the reconstruction using Boundary wavelet. The second row
-            is the difference between the original signal and the reconstruction
-            using mirrored extension. The third row is the first row minus the 
-            second row. there is one collumn for each section of the signal.
+            float64: 2D array. the firt row is the difference between
+            the original signal and the reconstruction using Boundary
+            wavelet. The second row is the difference between the
+            original signal and the reconstruction using mirrored
+            extension. The third row is the first row minus the second
+            row. there is one collumn for each section of the signal.
     '''
-    data    = sp.loadmat(Name)
-    phi     = pywt.Wavelet(Wavelet).wavefun(level=14)[0][1:]
-    phi     = phi[::2**(14-N)]
-    n       = 0
-    tests   = int(len(data['val'][Row])/2**N)
-    Result=np.zeros((3,tests))
+
+    data = sp.loadmat(Name)
+    phi = pywt.Wavelet(Wavelet).wavefun(level=14)[0][1:]
+    phi = phi[::2**(14-N)]
+    n = 0
+    tests = int(len(data['val'][Row])/2**N)
+    Result = np.zeros((3, tests))
     for i in range(tests):
-        Signal      = data['val'][Row,n:n+2**N]
-        x1          = RF.DecomBoundary(Signal,J,Wavelet,phi,N=N)
-        x2          = RF.DecomMirror(Signal,J,Wavelet,phi,N=N)
-        NewSignal1  = np.real(RF.ReconBoundary(x1,J,Wavelet,phi,N=N))
-        NewSignal2  = np.real(RF.ReconMirror(x2,J,Wavelet,phi,N=N))
-        Result[0,i] = np.sum(np.abs(Signal-NewSignal1)**2)**(1/2)/2**N
-        Result[1,i] = np.sum(np.abs(Signal-NewSignal2)**2)**(1/2)/2**N
-        n          += 2**N
+        Signal = data['val'][Row, n:n+2**N]
+        x1 = RF.DecomBoundary(Signal, J, Wavelet, phi, N=N)
+        x2 = RF.DecomMirror(Signal, J, Wavelet, phi, N=N)
+        NewSignal1 = np.real(RF.ReconBoundary(x1, J, Wavelet, phi, N=N))
+        NewSignal2 = np.real(RF.ReconMirror(x2, J, Wavelet, phi, N=N))
+        Result[0, i] = np.sum(np.abs(Signal-NewSignal1)**2)**(1/2)/2**N
+        Result[1, i] = np.sum(np.abs(Signal-NewSignal2)**2)**(1/2)/2**N
+        n += 2**N
     Result[2] = Result[0]-Result[1]
     plt.figure()
-    plt.plot(Result[1],label='Mirror',color='C1')
-    plt.plot(Result[0],label='Boundary',color='C0')
+    plt.plot(Result[1], label='Mirror', color='C1')
+    plt.plot(Result[0], label='Boundary', color='C0')
     plt.xlabel('Test signal')
     plt.ylabel('Difference')
     plt.legend()
     return Result
 
+
 TestPlot()
-Test=Test()
+Test = Test()
--- test/WaveletTest.py
+++ test/WaveletTest.py
@@ -3,7 +3,7 @@
 This module is used for testing and showing the boundary wavelets in the time
 and frequency domain.
 
-The BoundaryWavelets.py package is licensed under the MIT "Expat" 
+The BoundaryWavelets.py package is licensed under the MIT "Expat"
 Copyright (c) 2018: Josefine Holm and Steffen L. Nielsen.
 """
 # =============================================================================
@@ -15,111 +15,137 @@ import matplotlib.pyplot as plt
 import BoundaryWavelets as BW
 import FourierBoundaryWavelets as FBW
 import Orthonormal as Ot
+
+
 # =============================================================================
 # Functions
 # =============================================================================
-
-def GeneralTest(Wavelet='db2',J=2,epsilon=1/7,Length=1792,TimeOnly=False):
+def GeneralTest(Wavelet='db2', J=2, epsilon=1/7, Length=1792, TimeOnly=False):
     '''
-    A test function used in the paper. PLots the boundary wavelets in time and 
-    compare them with the boundary wavelets created in frequency by using an 
-    inverse Fourier transform. \n
+    A test function used in the paper. Plots the boundary wavelets in time and
+    compare them with the boundary wavelets created in frequency by using an
+    inverse Fourier transform.
+
     INPUT:
         Wavelet='db2':
             str: The wavelet to use in the test.
         J=2:
-            int: The scale. If J is too small for the scaling function to be supported
-            within the interval [0,1] it is changed to the smallest possible J.
+            int: The scale. If J is too small for the scaling function
+            to be supported within the interval [0,1] it is changed to
+            the smallest possible J.
         epsilon=1/7:
-            float: The sampling dencity in the frequency domain, i.e. the distance between
-            samples. In Generalized sampling this must be at least 1/7 for db2.
+            float: The sampling dencity in the frequency domain,
+            i.e. the distance between samples. In Generalized sampling
+            this must be at least 1/7 for db2.
         Length=1792:
-            int: The number of samples in the frequency domain. The standard is chosen
-            because 1792/7=256.
+            int: The number of samples in the frequency domain. The
+            standard is chosen because 1792/7=256.
         TimeOnly=False:
-            bool: If TImeOnly=True the boundary functions are only constructed in 
-            time, not in frequency.
+            bool: If TImeOnly=True the boundary functions are only
+            constructed in time, not in frequency.
     '''
+
     WaveCoef = np.flipud(pywt.Wavelet(Wavelet).dec_lo)
-    phi      = pywt.Wavelet(Wavelet).wavefun(level=15)[0][1:]
-    a        = int(len(WaveCoef)/2)
-    OneStep  = len(phi)//(2*a-1)
-    if J<a:
+    phi = pywt.Wavelet(Wavelet).wavefun(level=15)[0][1:]
+    a = int(len(WaveCoef)/2)
+    OneStep = len(phi)//(2*a-1)
+    if J < a:
         J = a
-        print('J has been changed to',J)
-    AL,AR    = Ot.OrthoMatrix(J,WaveCoef,phi)
-    phiNorm  = np.sqrt(1/OneStep*np.sum(np.abs(BW.DownSample(phi,0,OneStep,J)**2)))
-    BoundaryT= BW.BoundaryWavelets(phi/phiNorm,J,WaveCoef,AL=AL,AR=AR)
-    NormT    = np.zeros(2*a)
+        print('J has been changed to', J)
+    AL, AR = Ot.OrthoMatrix(J, WaveCoef, phi)
+    phiNorm = np.sqrt(1/OneStep*np.sum(np.abs(BW.DownSample(phi, 0,
+                                                            OneStep, J)**2)))
+    BoundaryT = BW.BoundaryWavelets(phi/phiNorm, J, WaveCoef, AL=AL, AR=AR)
+    NormT = np.zeros(2*a)
     for i in range(2*a):
-        NormT[i] = np.sqrt(1/OneStep*np.sum(np.abs(BoundaryT[:,i])**2))
+        NormT[i] = np.sqrt(1/OneStep*np.sum(np.abs(BoundaryT[:, i])**2))
     IP = 0
     for i in range(a):
         IP += i
     LeftInnerProdT = np.zeros(IP)
     ij = 0
     for i in range(a):
-        for j in range(i+1,a):
-            LeftInnerProdT[ij] = 1/OneStep*np.sum(BoundaryT[:,i]*BoundaryT[:,j])
+        for j in range(i+1, a):
+            LeftInnerProdT[ij] = 1/OneStep*np.sum(BoundaryT[:, i] *
+                                                  BoundaryT[:, j])
             ij += 1
     RightInnerProdT = np.zeros(IP)
     ij = 0
     for i in range(a):
-        for j in range(i+1,a):
-            RightInnerProdT[ij] = 1/OneStep*np.sum(BoundaryT[:,i+a]*BoundaryT[:,j+a])
+        for j in range(i+1, a):
+            RightInnerProdT[ij] = 1/OneStep*np.sum(BoundaryT[:, i+a] *
+                                                   BoundaryT[:, j+a])
             ij += 1
-    if TimeOnly==True:
+    if TimeOnly:
         print('Norms of functions in time', NormT)
-        print('Inner products in time',LeftInnerProdT,RightInnerProdT)
+        print('Inner products in time', LeftInnerProdT, RightInnerProdT)
         plt.figure()
         for i in range(a):
-            plt.plot(np.linspace(0,1,len(BoundaryT[:,i])),BoundaryT[:,i],label='$\phi^L_{%d,%d}$'%(J,i))
+            plt.plot(np.linspace(0, 1, len(BoundaryT[:, i])),
+                     BoundaryT[:, i], label=fr'$\phi^L_{{J},{i}}$')
         for i in range(a):
-            plt.plot(np.linspace(0,1,len(BoundaryT[:,i+a])),BoundaryT[:,i+a],label='$\phi^R_{%d,%d}$'%(J,i))
+            plt.plot(np.linspace(0, 1, len(BoundaryT[:, i+a])),
+                     BoundaryT[:, i+a], label=fr'$\phi^R_{{J},{i}}$')
         plt.legend()
         plt.xlabel('Time')
         plt.ylabel('Amplitude')
     else:
-        s         = int(Length*epsilon)
-        d         = 1/epsilon
-        Scheme    = np.linspace(-Length*epsilon/2,Length*epsilon/2,Length,endpoint=False)
-        BoundaryF = FBW.FourierBoundaryWavelets(J,Scheme,WaveCoef,AL=AL,AR=AR)
-        NormF     = np.zeros(2*a)
+        s = int(Length*epsilon)
+        d = 1/epsilon
+        Scheme = np.linspace(-Length*epsilon/2, Length*epsilon/2,
+                             Length, endpoint=False)
+        BoundaryF = FBW.FourierBoundaryWavelets(J, Scheme, WaveCoef,
+                                                AL=AL, AR=AR)
+        NormF = np.zeros(2*a)
         for i in range(2*a):
-            NormF[i]   = np.sqrt(epsilon*np.sum(np.abs(BoundaryF[:,i])**2))
-        LeftInnerProdF = np.zeros(IP,dtype=complex)
-        ij    = 0
-        for i in range(a):
-            for j in range(i+1,a):
-                LeftInnerProdF[ij] = epsilon*np.sum(BoundaryF[:,i]*BoundaryF[:,j])
-                ij += 1
-        RightInnerProdF = np.zeros(IP,dtype=complex)
+            NormF[i] = np.sqrt(epsilon*np.sum(np.abs(BoundaryF[:, i])**2))
+        LeftInnerProdF = np.zeros(IP, dtype=complex)
         ij = 0
         for i in range(a):
-            for j in range(i+1,a):
-                RightInnerProdF[ij] = epsilon*np.sum(BoundaryF[:,i+a]*BoundaryF[:,j+a])
+            for j in range(i+1, a):
+                LeftInnerProdF[ij] = epsilon*np.sum(BoundaryF[:, i] *
+                                                    BoundaryF[:, j])
+                ij += 1
+        RightInnerProdF = np.zeros(IP, dtype=complex)
+        ij = 0
+        for i in range(a):
+            for j in range(i+1, a):
+                RightInnerProdF[ij] = epsilon*np.sum(BoundaryF[:, i+a] *
+                                                     BoundaryF[:, j+a])
                 ij += 1
         print('Norms of functions in time', NormT)
-        print('Norms of functions i frequency',NormF)
-        print('Inner products in time',LeftInnerProdT,RightInnerProdT)
-        print('Inner products in frequency',LeftInnerProdF,RightInnerProdF)
-        InverseBoundaryF=np.zeros((s,2*a),dtype=complex)
+        print('Norms of functions i frequency', NormF)
+        print('Inner products in time', LeftInnerProdT, RightInnerProdT)
+        print('Inner products in frequency', LeftInnerProdF, RightInnerProdF)
+        InverseBoundaryF = np.zeros((s, 2*a), dtype=complex)
         for i in range(2*a):
-            InverseBoundaryF[:,i] = d**(1/2)*np.fft.ifft(d**(-1/2)*np.concatenate((BoundaryF[len(BoundaryF[:,i])//2:,i],BoundaryF[:len(BoundaryF[:,i])//2,i])),norm='ortho')[:s]
-            Fnorm=np.sqrt(1/256*np.sum(np.abs(InverseBoundaryF[:,i])**2))
-            InverseBoundaryF[:,i]/=Fnorm
+            InverseBoundaryF[:, i] = d**(1/2)*np.fft.ifft(
+                d**(-1/2) * np.concatenate(
+                    (BoundaryF[len(BoundaryF[:, i])//2:, i],
+                     BoundaryF[:len(BoundaryF[:, i])//2, i])),
+                norm='ortho')[:s]
+            Fnorm = np.sqrt(1/256*np.sum(np.abs(InverseBoundaryF[:, i])**2))
+            InverseBoundaryF[:, i] /= Fnorm
         plt.figure()
-        plt.plot(np.linspace(0,1,len(BoundaryT[:,0])),BoundaryT[:,0],label='Scaling functions in time',color='C0')
-        plt.plot(np.linspace(0,1,s),np.real(InverseBoundaryF[:,0]),label='Scaling functions in frequency',color='C1')
-        for i in range(1,a):
-            plt.plot(np.linspace(0,1,len(BoundaryT[:,0])),BoundaryT[:,i],color='C0')
-            plt.plot(np.linspace(0,1,s),np.real(InverseBoundaryF[:,i]),color='C1')
+        plt.plot(np.linspace(0, 1, len(BoundaryT[:, 0])),
+                 BoundaryT[:, 0], label='Scaling functions in time',
+                 color='C0')
+        plt.plot(np.linspace(0, 1, s), np.real(InverseBoundaryF[:, 0]),
+                 label='Scaling functions in frequency', color='C1')
+        for i in range(1, a):
+            plt.plot(np.linspace(0, 1, len(BoundaryT[:, 0])),
+                     BoundaryT[:, i], color='C0')
+            plt.plot(np.linspace(0, 1, s), np.real(InverseBoundaryF[:, i]),
+                     color='C1')
         for i in range(a):
-            plt.plot(np.linspace(0,1,len(BoundaryT[:,0])),BoundaryT[:,i+a],color='C0')
-            plt.plot(np.linspace(0,1,s),np.real(InverseBoundaryF[:,i+a]),color='C1')
+            plt.plot(np.linspace(0, 1, len(BoundaryT[:, 0])),
+                     BoundaryT[:, i+a], color='C0')
+            plt.plot(np.linspace(0, 1, s), np.real(InverseBoundaryF[:, i+a]),
+                     color='C1')
         plt.legend()
         plt.xlabel('Time')
         plt.ylabel('Amplitude')
-    return 
+    return
 
-GeneralTest(Wavelet='db2',J=2,TimeOnly=False)
+
+GeneralTest(Wavelet='db2', J=2, TimeOnly=False)
